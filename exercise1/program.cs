9f6b80b8f8f4e3bc6232183a9893468e8f4636e8
daphineayaa <ayaadaphine0@gmail.com>
2025-07-17 04:29:54 -0700

    Initial commit: Add C# program

---
+++ exercise1/program.cs
@@ -0,0 +1,10 @@
+// Prompt for first name
+Console.Write("What is your first name? ");
+string firstName = Console.ReadLine();
+
+// Prompt for last name
+Console.Write("What is your last name? ");
+string lastName = Console.ReadLine();
+
+// Display the formatted output
+Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
